56a56adf8467d514da1f4a95acc7952aeece01cd
çağrı çamyar <cagri-95@hotmail.com>
2024-07-12 16:30:39 +0300

    Angular 2. ders sonu Backend ile iliskilendirildi getall fonksiyonu islendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
-import { ProductResponceModel } from '../../models/productResponseModel';
- 
+import { ProductService } from '../../services/product.service';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
-export class ProductComponent {
- 
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
 
-  products:Product[] = []; 
+  constructor(private productService:ProductService) {}
+
+  ngOnInit(): void {
+  this.getProducts()  
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+    })
+  }
+
+  
+  
 }
