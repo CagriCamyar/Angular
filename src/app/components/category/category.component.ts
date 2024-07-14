c1be7d9d3711f67aae005b9ce72e3a55f4ff1cea
çağrı çamyar <cagri-95@hotmail.com>
2024-07-14 19:55:21 +0300

    Angular Ders 3 Sonu

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories : Category[] = [];
+  currentCategory:Category;
+ 
+  constructor(private categoryService:CategoryService) { }
+  
+  ngOnInit(): void {
+    this.getCategories();
+    }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories=response.data
+    })
+  }
+  
+  setCurrentCategory(category:Category){
+  this.currentCategory=category
+  }
+
+  getCurrentCategoryClass(Category:Category){
+    if(Category == this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+
+  getAllCategoryClass(){
+    if (!this.currentCategory){
+      return"list-group-item active"
+    }
+    else {
+      return "list-group-item"
+    }
+  }
 
 }
