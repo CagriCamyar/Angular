9bfd1f23c8ccf8492b40fe9f10e7936a80fed22b
çağrı çamyar <cagri-95@hotmail.com>
2024-07-19 18:33:17 +0300

    Login ve Guard Eklendi 6. ders Sonu

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+import { ToastrService } from 'ngx-toastr';
+import { response } from 'express';
 
 @Component({
   selector: 'app-product-add',
@@ -9,7 +12,7 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class ProductAddComponent implements OnInit {
 
   productAddForm:FormGroup;
-constructor(private formBuilder:FormBuilder) {
+constructor(private formBuilder:FormBuilder, private productService:ProductService, private toastrService:ToastrService) {
   
 }
 
@@ -25,5 +28,24 @@ createProductAddForm(){
     categoryId:["",Validators.required]
   })
 }
+
+add(){
+  if(this.productAddForm.valid){
+    let productModel = Object.assign({}, this.productAddForm.value) 
+    this.productService.add(productModel).subscribe(response=>{
+      this.toastrService.success(response.message, "Basarili")
+     }, responseError=>{
+      if(responseError.error.Errors.lenght>0){
+        for (let i = 0; i < responseError.error.Errors.lenght; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Dogrulama Hatasi")
+
+        }
+      }
+     } )
+  }else{
+    this.toastrService.error("Formunuz Eksik", "Dikkat")
+  }
+
+} 
 
 }
